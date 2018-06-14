ebc3929bafe95396810fe3f35fab2cdf73827f83
Sergey Vlasovv <35797418+SergeyVlasov@users.noreply.github.com>
2018-06-14 23:19:20 +0300

    Update Q-learning.py

--- Q-learning.py
+++ Q-learning.py
@@ -1,40 +1,35 @@
 import numpy
 import pandas
 import time
- 
-numpy.random.seed(2)
- 
- 
-N_Stapes = 10                    #ðàçìåð îñòðîâà
-FIRST_STATE = 5                 #îòêóäà íà÷èíàåì ãóëÿòü
+
+
+N_STATES = 10
+FIRST_STATE = 5
 ACTIONS = ['left', 'right']
-EPSILON = 0.7                     # ïîðîã ïðèíÿòèÿ ðåøåíèÿ êàê âûáðàòü äåéñòâèå
-ALPHA = 0.3                         # ëåàðíèíã ðýéò
-GAMMA = 0.8                       # äèñêîíòèðóþùèé ôàêòîð
-MAX_EPISODES = 10             # ÷èñëî ýïèçîäîâ (ýïèçîä ýòî êîãäà äîñòèãëè ïðîïàñòè èëè ñîêðîâèùà
-FRESH_TIME = 0.05                # ñêîðîñòü ïåðåðèñîâêè
+EPSILON = 0.7
+ALPHA = 0.3
+GAMMA = 0.8
+MAX_EPISODES = 10
+FRESH_TIME = 0.05
 FRESH_TIME_EPIS = 1
- 
- 
-def build_q_table(n_states, actions):     # ñîáñòâåííî Q- òàáëèöà
+INCREASE_EPSILON_KOEFF = 1
+
+def build_q_table(n_states, actions):
     table = pandas.DataFrame(numpy.zeros((n_states, len(actions))), columns=actions,)
     return table
- 
- 
-def choose_action(state, q_table):             # âûáîð äåéñòâèÿ, èñïîëüçóåì ýïñèëîí-greedy policy òî åñòü ñ âåðîÿòíîñòüþ ýïñèëîí âûáåðåì äåéñòâèå ñ áîëüøåé öåííîñòüþ
+
+
+def choose_action(state, q_table):             # e-greedy policy
     state_actions = q_table.iloc[state, :]
-    if (numpy.random.uniform() > EPSILON) or (state_actions.all() == 0):
-        action_name = numpy.random.choice(ACTIONS)
-    else:
-        action_name = state_actions.argmax()
+    action_name = numpy.random.choice(ACTIONS) if ((numpy.random.uniform() > EPSILON) or (state_actions.all() == 0)) else state_actions.argmax()
     return action_name
- 
- 
-def get_env_feedback(S, A):  # Íàãðàäà
+
+
+def env_feedback(S, A):  # Награда
     if A == 'right':
-        if S == N_Stapes-1:
+        if S == N_STATES-1:
             S_ = 'terminal'
-            R = 1    # íàãðàäà â ñîêðîâèùå
+            R = 1    # treasure
         else:
             S_ = S + 1
             R = 0
@@ -42,61 +37,61 @@ def get_env_feedback(S, A):  # Íàãðàäà
        # R = 0
         if S == 0:
             S_ = 'abyss'
-            R = -1   # íàãðàäà â ïðîïàñòè
- 
+            R = -1   # abbys
+
         else:
             S_ = S - 1
             R = 0
     return S_, R
- 
- 
-def update_enviroment(S, episode, step_counter):   # ïåðåðèñîâêà "îñòðîâà"
-    env_list = ['abyss:']+['-']*(N_Stapes-2) + [':treasure']
+
+
+def update_enviroment(S, episode, step_counter):   # update the land
+    env_list = ['abyss:']+['-']*(N_STATES-2) + [':treasure']
     if S == 'terminal' or S == 'abyss':
         interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
         print('\r{}'.format(interaction), end='')
         time.sleep(FRESH_TIME_EPIS)
         print('\r                                ', end='')
- 
+
     else:
         env_list[S] = '(o.0)'
         interaction = ''.join(env_list)
         print('\r{}'.format(interaction), end='')
         time.sleep(FRESH_TIME)
- 
- 
+
+
 def reinforsment_learning():
-    q_table = build_q_table(N_Stapes, ACTIONS)
+    q_table = build_q_table(N_STATES, ACTIONS)
     for episode in range(MAX_EPISODES):
         step_counter = 0
         S = FIRST_STATE
         is_terminated = False
         update_enviroment(S, episode, step_counter)
         while is_terminated == False:
-            A = choose_action(S, q_table)                    # âûáðàëè äåéñòâèå â ñîîòâåòñòâóþùåì ñòåéòå
-            S_, R = get_env_feedback(S, A)                   #  ïîëó÷èëè îòçûâ ñðåäû íà ýòî äåéñòâèå (ñëåäóþùåå äåéñòâèå è íàãðàäó)
-            q_predict = q_table.ix[S, A]                     #  ñóùåñòâóþùåå çíà÷åíèå öåííîñòè äåéñòâèÿ èç ñóùåñòâóþùåé òàáëèöû
+            A = choose_action(S, q_table)                    # выбрали действие в соответствующем стейте
+            S_, R = env_feedback(S, A)                   #  получили отзыв среды на это действие (следующее действие и награду)
+            q_predict = q_table.ix[S, A]                     #  существующее значение ценности действия из существующей таблицы
             if S_ != 'terminal' and S_ != 'abyss':
- 
-                # èçìåíåíèå âåñîâ òàáëèöû:
-                if A == 'right': # äëÿ òîãî ÷òîáû âåñà èçìåíÿëèñü ñ ó÷åòîì òîãî ÷òî ñïðàâà - ñîêðîâèùà, ñëåâà - ïðîïàñòü
-                    q_target = R + GAMMA * q_table.iloc[S_, :].max()  # ïîëó÷åííàÿ öåííîñòü äåéñòâèÿ ðàâíà íàãðàäå + ìàêñèìàëüíîå çíà÷åíèå öåííîñòè äåéñòâèÿ â ñëåäóþùåì ñòåéòå!!!!!????
+                # changing "weights"
+                if A == 'right': # для того чтобы веса изменялись с учетом того что справа - сокровища, слева - пропасть
+                    q_target = R + GAMMA * q_table.iloc[S_, :].max()  # полученная ценность действия равна награде + максимальное значение ценности действия в следующем стейте!!!!!????
                 else:
                     q_target = R + GAMMA * q_table.iloc[S_, :].min()
- 
             else:
                 q_target = R
                 is_terminated = True
             q_table.ix[S, A] += ALPHA * (q_target - q_predict)
             S = S_
- 
- 
             update_enviroment(S, episode, step_counter+1)
             step_counter += 1
-        
+
+    global EPSILON     # decrease_epsilon
+    if EPSILON <=1:
+        EPSILON *= INCREASE_EPSILON_KOEFF
+
     return q_table
- 
- 
+
+
 q_table = reinforsment_learning()
 print('\r\nQ-table:\n')
 print(q_table)
